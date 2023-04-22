12c677c958cdde376e8406eba942a5f708457309
Enochthedev <enochjesse884@gmail.com>
2023-04-22 23:11:16 +0100

    added package checker

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const http = require('http');
 const app = require('./src/app');
+const packageJson = require('./package.json');
+const { exec } = require('child_process');
 
-const port = process.env.PORT || 3000;
+const requiredDependencies = Object.keys(packageJson.dependencies);
 
-const server = http.createServer(app);
+// Check if all required dependencies are installed
+exec('npm ls --depth=0 --json', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error checking dependencies: ${error.message}`);
+        process.exit(1);
+    }
 
-server.listen(port , () => {
-    console.log('Server is running on port ' + port);
-    console.log('http://localhost:' + port)
-    
+    const installedDependencies = Object.keys(JSON.parse(stdout).dependencies);
+
+    const missingDependencies = requiredDependencies.filter((dependency) => !installedDependencies.includes(dependency));
+
+    if (missingDependencies.length > 0) {
+        console.error(`Missing dependencies: ${missingDependencies.join(', ')}`);
+        console.error(`Please install the missing dependencies by running 'npm install' before starting the server.`);
+        process.exit(1);
+    }
+
+    // Start the server
+    const port = process.env.PORT || 3000;
+
+    const server = http.createServer(app);
+
+    server.listen(port, () => {
+        console.log('All dependencies installed successfully')
+        console.log('-'.repeat(80) + '\n')
+        console.log('Server starting...')
+        console.log('Server started');
+        console.log('Server name: ' + packageJson.name);
+        console.log('Server version: ' + packageJson.version);
+        console.log('Server is running on port ' + port);
+        console.log('http://localhost:' + port);
+    });
 });
